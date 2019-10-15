1c72c832ae22ceed98df05b3dca1f2a9b1b72a11
terfno <sueda.takahito@gmail.com>
2019-10-15 14:26:17 +0900

    :zap: add explain about package importing

--- 2_package/2_1_rand.go
+++ 2_package/2_1_rand.go
@@ -5,6 +5,10 @@ import (
 	"math/rand"
 )
 
+// ↑と↓は同義だが、↑の書き方が推奨される。`$ go fmt`とかやると↑になる。
+// import "fmt"
+// import "math/rand"
+
 func main() {
 	fmt.Println("random number: ", rand.Intn(100))
 }
